a442926b647b3dc8ef543309dbc9617d622228ff
Arturo Suárez <manuel.suarez@cimat.mx>
2023-05-11 13:01:00 -0600

    Metrics

--- main.py
+++ main.py
@@ -162,3 +162,23 @@ for epoch in range(num_epochs):
 
         # update tqdm loop
         loop.set_postfix(loss=loss.item())
+
+def check_accuracy(loader, model):
+    num_correct = 0
+    num_pixels = 0
+    dice_score = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(DEVICE)
+            y = y.to(DEVICE)
+            softmax = nn.Softmax(dim=1)
+            preds = torch.argmax(softmax(model(x)),axis=1)
+            num_correct += (preds == y).sum()
+            num_pixels += torch.numel(preds)
+            dice_score += (2 * (preds * y).sum()) / ((preds + y).sum() + 1e-8)
+
+    print(f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}")
+    print(f"Dice score: {dice_score/len(loader)}")
+    model.train()
