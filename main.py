c67c73b281ff4d8770f947d0126cd079410123d4
Arturo Suárez <manuel.suarez@cimat.mx>
2023-05-11 12:46:49 -0600

    Initial setup

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from albumentations.pytorch import ToTensorV2
 import albumentations as A
+
+import torch.nn as nn
+from torch.optim import Adam
+from tqdm import tqdm
